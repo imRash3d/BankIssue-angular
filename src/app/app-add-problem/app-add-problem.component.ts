b9d0affe60179f4cf8d68fbf269c1b2576c12f4c
Md Rashedul Islam <rashedul.islam@selise.ch>
2022-03-03 13:57:46 +0600

    fix design

--- src/app/app-add-problem/app-add-problem.component.ts
+++ src/app/app-add-problem/app-add-problem.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-add-problem',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppAddProblemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  cancel(){
+    this.router.navigateByUrl('/list')
+  }
 }
